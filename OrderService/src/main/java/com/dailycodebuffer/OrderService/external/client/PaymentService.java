9146ef61e1eb718f284fe74ede596911fc3ec341
bhuvanye <bhuvan.y@hevodata.com>
2023-05-16 08:02:36 +0530

    Completed all services

--- OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/PaymentService.java
@@ -1,14 +1,21 @@
 package com.dailycodebuffer.OrderService.external.client;
 
+import com.dailycodebuffer.OrderService.Exception.CustomException;
 import com.dailycodebuffer.OrderService.external.Request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e){
+        throw new CustomException("Payment service is down", 500, "UNAVAILABLE");
+    }
 }
